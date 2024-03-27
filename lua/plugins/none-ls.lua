375c1414bf7ae8fd7fd719fc876fc03117fc8d19
Yash Shah <yashshah04.03.2002@outlook.com>
2024-03-27 02:42:43 +0000

    Added linting + formatting support and auto-installing linters and formatters through mason-null-ls.nvim

---
+++ lua/plugins/none-ls.lua
@@ -0,0 +1,30 @@
+return {
+  {
+    "nvimtools/none-ls.nvim",
+    config = function()
+      local null_ls = require("null-ls")
+
+      null_ls.setup({
+        sources = {
+          null_ls.builtins.formatting.stylua,
+          null_ls.builtins.formatting.black,
+          null_ls.builtins.formatting.isort,
+        },
+      })
+    end
+  },
+  {
+    "jay-babu/mason-null-ls.nvim",
+    event = { "BufReadPre", "BufNewFile" },
+    dependencies = {
+      "williamboman/mason.nvim",
+      "nvimtools/none-ls.nvim",
+    },
+    config = function()
+      require("mason-null-ls").setup({
+        ensure_installed = nil,
+        automatic_installation = true,
+      })
+    end,
+  },
+}
