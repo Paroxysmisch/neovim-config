3f64008a363fc6d209ccaa9baef919e42a058762
Yash Shah <yashshah04.03.2002@outlook.com>
2024-03-30 03:55:38 +0000

    Added mini.nvim for textobjects and surround

--- lua/plugins/other.lua
+++ lua/plugins/other.lua
@@ -2,4 +2,23 @@ return {
   'tpope/vim-sleuth', -- Detect tabstop and shiftwidth automatically,
   -- "gc" to comment visual regions/lines
   { 'numToStr/Comment.nvim', opts = {} },
+  {
+    'echasnovski/mini.nvim',
+    config = function()
+      -- Better Around/Inside textobjects
+      --
+      -- Examples:
+      --  - va)  - [V]isually select [A]round [)]paren
+      --  - yinq - [Y]ank [I]nside [N]ext [']quote
+      --  - ci'  - [C]hange [I]nside [']quote
+      require('mini.ai').setup { n_lines = 500 }
+
+      -- Add/delete/replace surroundings (brackets, quotes, etc.)
+      --
+      -- - saiw) - [S]urround [A]dd [I]nner [W]ord [)]Paren
+      -- - sd'   - [S]urround [D]elete [']quotes
+      -- - sr)'  - [S]urround [R]eplace [)] [']
+      require('mini.surround').setup()
+    end
+  }
 }
