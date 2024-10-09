37b6aac5ad008e2df7399d928ebdf314dcf963e8
Yash Shah <yashshah00@outlook.com>
2024-10-09 14:33:38 +0100

    Added a shortcut to format the current buffer using LSP.

--- init.lua
+++ init.lua
@@ -102,6 +102,9 @@ vim.keymap.set('n', '<C-l>', '<C-w><C-l>', { desc = 'Move focus to the right win
 vim.keymap.set('n', '<C-j>', '<C-w><C-j>', { desc = 'Move focus to the lower window' })
 vim.keymap.set('n', '<C-k>', '<C-w><C-k>', { desc = 'Move focus to the upper window' })
 
+-- Use attached LSP to format
+vim.keymap.set('n', '<leader>f', vim.lsp.buf.format, { desc = 'Format current buffer using attached LSP' })
+
 -- [[ Basic Autocommands ]]
 --  See `:help lua-guide-autocommands`
 
